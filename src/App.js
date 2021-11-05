2d2f9576121d1907704bf6b5d3420f9699badc08
JessicaLima <jessikurt@gmail.com>
2021-11-05 15:26:38 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './page/Home';
+import GlobalStyle from './styles/globalStyles';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
