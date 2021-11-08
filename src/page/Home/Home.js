0a8395914eea14b6d9cffbfec424e8f97892f821
JessicaLima <jessikurt@gmail.com>
2021-11-08 10:59:13 -0300

    feat: create Home on Page

---
+++ src/page/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+import { Container } from './styles'
+
+const Home = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  )
+}
+
+export default Home
