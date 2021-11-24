a6a516a43d1477a4e83b2a186d5860918da2e3a5
JessicaLima <jessikurt@gmail.com>
2021-11-24 15:31:32 -0300

    SecureInternetCertificate

---
+++ src/components/SecureInternetCertificate/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    color: #597056;
+    background: none;
+    border: 1px solid #888888;
+    border-radius: 50%;
+    width: 1rem;
+    height: 1rem;
+    padding: 2px;
+  }
+  h6 {
+    font-weight: 400;
+    margin: 0 0.5rem;
+    color: #888888;
+  }
+  a {
+    color: #7b8f66;
+  }
+`;
