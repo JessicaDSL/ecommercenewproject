0b09e004ffdb9bfb6c291a691a1b4df25c89e05c
JessicaLima <jessikurt@gmail.com>
2021-11-24 15:33:14 -0300

    feat: create SignToBankConfirm Component

---
+++ src/components/SignToBankConfirm/SignToBankConfirm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import HeaderSignBank from "../HeaderSignBank/HeaderSignBank";
+import { Background, Container, List, ListItem, Label, Input } from "./styles";
+import SecureInternetCertificate from '../SecureInternetCertificate'
+//"Choose an account"
+
+const SignToBankConfirm = () => {
+  const title = "Sign into your bank";
+
+  return (
+    <Background>
+      <HeaderSignBank title={title} />
+      <Container>
+        <h2>
+          shoes<strong>.on</strong>
+        </h2>
+        <p>About signing into Online Banking</p>
+        <List>
+          <ListItem>This is a secure connection to Chase</ListItem>
+          <ListItem>Your bank login cannot be seen by shoes.on</ListItem>
+        </List>
+        <Label>
+          <Input type="checkbox" />
+          <span>Do not show this message again</span>
+        </Label>
+        <button>Ok, got it!</button>
+      </Container>
+      <SecureInternetCertificate />
+    </Background>
+  );
+};
+
+export default SignToBankConfirm;
