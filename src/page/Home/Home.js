095ee0244f62f3db8f70c27c32c6dc7d8e96e528
JessicaLima <jessikurt@gmail.com>
2021-12-06 18:14:10 -0300

    feat: excluded content file

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,28 +1,16 @@
 import React from "react";
 
-import CheckoutPayment from '../../components/CheckoutPayment'
-//import { results } from "./content";
+import ProductList from "../../components/ProductList";
+import { ProductProvider } from "../../context/ContextApi";
 
 import { Container } from "./styles";
-const product = [
-  {
-    id: "2839u512401",
-    description: "SS Sneaker",
-    color: "Green",
-    price: "100.00",
-    currency: "USD",
-    thumbnailURL:
-      "https://voliveira.s3-sa-east-1.amazonaws.com/sneakers/ss-sneaker-thumb.png",
-    maxresURL:
-      "https://voliveira.s3-sa-east-1.amazonaws.com/sneakers/ss-sneaker-maxres.png",
-  },
-];
-
 const Home = () => {
   return (
-    <Container>
-      <CheckoutPayment product={product} />
-    </Container>
+    <ProductProvider>
+      <Container>
+        <ProductList />
+      </Container>
+    </ProductProvider>
   );
 };
 
