fa06eef73b611dc818dcda76b957d210afa47b88
JessicaLima <jessikurt@gmail.com>
2021-12-06 18:14:40 -0300

    feat: created routes on App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import CheckoutPayment from './components/CheckoutPayment/CheckoutPayment';
+import { ProductProvider } from './context/ContextApi';
 import Home from './page/Home';
 import GlobalStyle from './styles/globalStyles';
 
 function App() {
   return (
-    <div>
+    <ProductProvider>
+      <Router>
       <GlobalStyle />
-      <Home />
-    </div>
+      <Routes>
+        <Route  path="/" element={<Home />}/>
+        <Route  path="productcart" element={<CheckoutPayment />}/>
+      </Routes>
+    </Router>
+    </ProductProvider>
   );
 }
 
