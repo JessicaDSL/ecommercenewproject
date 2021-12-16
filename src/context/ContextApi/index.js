76f4e16b459e200e93c8654b741fc45aecc86ff8
JessicaLima <jessikurt@gmail.com>
2021-12-16 16:51:20 -0300

    feat: realocaded api

--- src/context/ContextApi/index.js
+++ src/context/ContextApi/index.js
@@ -1,63 +1,34 @@
-import React, { createContext, useEffect, useState } from "react";
-import api from "../../services/api";
+import React, { createContext, useState } from "react";
 export const ProductContext = createContext({});
 
 export const ProductProvider = ({ children }) => {
-  
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
-  const [sizeProduct, setSizeProduct] = useState();
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const fetchProduct = () => {
-    return api
-      .get("sneakers/index.json")
-      .then(({ data }) => {
-        const products = data?.results?.map((item) => {
-          const id = item?.id;
-          const description = item?.description;
-          const color = item?.color;
-          const price = parseInt(item?.price);
-          const thumbnailURL = item?.thumbnailURL;
-          const maxresURL = item?.maxresURL;
-          const size = [37, 38, 39, 40, 41, 45, 46];
-          const quantity = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-          return {
-            id,
-            description,
-            color,
-            price,
-            thumbnailURL,
-            maxresURL,
-            size,
-            quantity,
-          };
-        });
-        return setProducts(products);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    fetchProduct();
-  }, []);
-
-  function handleGetSizeOfProduct(e) {
-    setSizeProduct(e.target.value);
+  function handleAddNewProduct(
+    quantityValue,
+    color,
+    sizeProduct,
+    id,
+    description,
+    photo,
+    totalResult
+  ) {
+    const product = {
+      quantity: quantityValue,
+      color: color,
+      size: sizeProduct,
+      id: id,
+      name: description,
+      photo: photo,
+      price: totalResult,
+    };
+    setCart([...cart, product]);
   }
 
-  function handleAddNewProduct(quantityValue, color, sizeProduct, id, description, photo, totalResult) {
-    const product =
-      {
-        quantity: quantityValue,
-        color: color,
-        size: sizeProduct,
-        id: id,
-        name: description,
-        photo: photo,
-        price: totalResult,
-      }
-    setCart([ ...cart, product ]);
+  function handleDeleteItem(item) {
+    const newListProduct = cart.filter((list) => list.id !== item.id);
+    setCart(newListProduct);
   }
 
   return (
@@ -65,9 +36,9 @@ export const ProductProvider = ({ children }) => {
       value={{
         cart,
         products,
-        sizeProduct,
-        handleGetSizeOfProduct,
+        setProducts,
         handleAddNewProduct,
+        handleDeleteItem,
       }}
     >
       {children}
