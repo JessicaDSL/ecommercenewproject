fe7634a53b6711f349547cbc4e06a94aebfd2edd
JessicaLima <jessikurt@gmail.com>
2021-12-16 16:49:45 -0300

    feat: add productcontext

--- src/components/ProductCart/ProductCart.js
+++ src/components/ProductCart/ProductCart.js
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MdOutlineAttachMoney } from "react-icons/md";
 import { FaMinusCircle } from "react-icons/fa";
 
+import { ProductContext } from "../../context/ContextApi";
+
 import { Container, Background, Image, Description, Price } from "./styles";
 
-const ProductCart = ({ description, id, color, price, photo }) => {
+const ProductCart = ({ item }) => {
+
+  const {handleDeleteItem} = useContext(ProductContext)
+
+
+
   return (
     <Background>
       <Container>
         <Image>
-          <img src={photo} alt={description} />
+          <img src={item.photo} alt={item.description} />
         </Image>
         <Description>
-          <h3>{description}</h3>
-          <p>x1, {color}, Size 41 </p>
-          <p>Item #{id}</p>
+          <h3>{item.description}</h3>
+          <p>x{item.quantity}, {item.color}, Size {item.size} </p>
+          <p>Item #{item.id}</p>
           <h5>Delivery Details</h5>
           <p>John Smiths</p>
           <p>Phone no: 01312428200</p>
           <p>Address: Redwood City, 2000.</p>
         </Description>
-        <span>
+        <button onClick={() => handleDeleteItem(item)}>
           <FaMinusCircle />
-        </span>
+        </button>
       </Container>
       <Price>
         <p>
@@ -31,7 +38,7 @@ const ProductCart = ({ description, id, color, price, photo }) => {
         </p>
         <strong>
           <MdOutlineAttachMoney />
-          {price}
+          {item.price}
         </strong>
       </Price>
     </Background>
