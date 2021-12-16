b4c1a77cbd21e87a50ad3606e3605926e64e115e
JessicaLima <jessikurt@gmail.com>
2021-12-16 16:50:05 -0300

    feat: add api

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 
+import api from '../../services/api'
 import ProductList from "../../components/ProductList";
-import { ProductProvider } from "../../context/ContextApi";
 
 import { Container } from "./styles";
+import { ProductContext } from "../../context/ContextApi";
+
 const Home = () => {
+
+  const {setProducts} = useContext(ProductContext)
+
+  const fetchProduct = () => {
+    return api
+      .get("sneakers/index.json")
+      .then(({ data }) => {
+        const productList = data?.results?.map((item) => {
+          const id = item?.id;
+          const description = item?.description;
+          const color = item?.color;
+          const price = parseInt(item?.price);
+          const thumbnailURL = item?.thumbnailURL;
+          const maxresURL = item?.maxresURL;
+          const size = [37, 38, 39, 40, 41, 45, 46];
+          const quantity = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+          return {
+            id,
+            description,
+            color,
+            price,
+            thumbnailURL,
+            maxresURL,
+            size,
+            quantity,
+          };
+        });
+        return setProducts(productList);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+  
   return (
-    <ProductProvider>
-      <Container>
-        <ProductList />
-      </Container>
-    </ProductProvider>
+    <Container>
+      <ProductList />
+    </Container>
   );
 };
 
