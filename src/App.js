e404105c1e8bf0042736726e4c6f7a9111f9ae74
JessicaLima <jessikurt@gmail.com>
2021-12-28 22:02:07 -0300

    feat: indentation for app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
-import CheckoutPayment from './components/CheckoutPayment/CheckoutPayment';
-import SignToBankConfirm from './components/SignToBankConfirm/SignToBankConfirm';
-import SelectBank from './components/SelectBank';
-import ChooseAccount from './components/ChooseAccount'
-import { ProductProvider } from './context/ContextApi';
-import Home from './page/Home';
-import GlobalStyle from './styles/globalStyles';
-import SignBankLogin from './components/SignBankLogin/SignBankLogin';
-import PaymentConfirmation from './components/PaymentConfirmation/PaymentConfirmation';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./page/Home";
+import Cart from "./page/Cart/Cart";
+import SelectBank from "./components/SelectBank";
+import SignToBankConfirm from "./components/SignToBankConfirm";
+import ChooseAccount from "./components/ChooseAccount";
+import GlobalStyle from "./styles/globalStyles";
+import SignBankLogin from "./components/SignBankLogin/SignBankLogin";
+import PaymentConfirmation from "./components/PaymentConfirmation";
 
 function App() {
   return (
-    <ProductProvider>
-      <Router>
+    <Router>
       <GlobalStyle />
       <Routes>
-        <Route  path="/" element={<Home />}/>
-        <Route  path="productcart" element={<CheckoutPayment />}/>
-        <Route  path="/selectbank" element={<SelectBank />}/>
-        <Route  path="/continue" element={<SignToBankConfirm />}/>
-        <Route  path="/signin" element={<SignBankLogin />}/>
-        <Route  path="/chooseaccount" element={<ChooseAccount />}/>
-        <Route  path="/confirm" element={<PaymentConfirmation />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart/" element={<Cart />} />
+        <Route path="/selectbank" element={<SelectBank />} />
+        <Route path="/continue" element={<SignToBankConfirm />} />
+        <Route path="/signin" element={<SignBankLogin />} />
+        <Route path="/chooseaccount" element={<ChooseAccount />} />
+        <Route path="/confirm" element={<PaymentConfirmation />} />
       </Routes>
     </Router>
-    </ProductProvider>
   );
 }
 
