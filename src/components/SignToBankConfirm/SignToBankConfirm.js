03fd5b87f95eb583efa0405b6cd6164c2493ed05
JessicaLima <jessikurt@gmail.com>
2021-12-30 16:36:07 -0300

    feat: implemented form with post method

--- src/components/SignToBankConfirm/SignToBankConfirm.js
+++ src/components/SignToBankConfirm/SignToBankConfirm.js
@@ -1,16 +1,34 @@
-import React from "react";
-import HeaderSignBank from "../HeaderSignBank/HeaderSignBank";
+import React, { useState, useEffect } from "react";
+
 import { Background, Container, List, ListItem, Label, Input } from "./styles";
 import SecureInternetCertificate from '../SecureInternetCertificate'
 import { Link } from "react-router-dom";
+import HeaderModal from "../HeaderModal";
 //"Choose an account"
 
 const SignToBankConfirm = () => {
   const title = "Sign into your bank";
+  const link = '/confirm'
+
+  const [state, setState] = useState(false)
+  const [active, setActive] = useState(false)
+
+  console.log(state)
+
+  function handleCkeck(e) {
+    console.log('e', e.target.checked)
+    setState(e.target.checked)
+  }
+
+  useEffect(() => {
+    setActive(true)
+    console.log('active', active)
+  }, [state, active])
+  
 
   return (
     <Background>
-      <HeaderSignBank title={title} />
+      <HeaderModal title={title} />
       <Container>
         <h2>
           shoes<strong>.on</strong>
@@ -20,13 +38,15 @@ const SignToBankConfirm = () => {
           <ListItem>This is a secure connection to Chase</ListItem>
           <ListItem>Your bank login cannot be seen by shoes.on</ListItem>
         </List>
+        <form method="POST" >
         <Label>
-          <Input type="checkbox" />
+          <Input type="checkbox" onChange={(e) => handleCkeck(e)}/>
           <span>Do not show this message again</span>
         </Label>
         <Link to='/signin'>
-        <button>Ok, got it!</button>
+        <button type="subimit" disabled={!state} active={active}>Ok, got it!</button>
         </Link>
+        </form>
       </Container>
       <SecureInternetCertificate />
     </Background>
