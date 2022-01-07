858405700936547d97d98ffceda30fede3a23259
JessicaLima <jessikurt@gmail.com>
2022-01-07 20:15:21 -0300

    refactor: change name props

--- src/context/ContextApi/index.js
+++ src/context/ContextApi/index.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
+
 export const ProductContext = createContext({});
 
 export const ProductProvider = ({ children }) => {
@@ -6,31 +7,38 @@ export const ProductProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [valor, setValor] = useState([0]);
   const [amount, setAmount] = useState([0]);
+  const [open, setOpen] = useState();
+  const [title, setTitle] = useState();
+  const [cartItem, setCartItem] = useState()
+
+ 
 
   function handleAddNewProduct(
     quantityValue,
-    color,
+    colorOfProduct,
     sizeProduct,
-    id,
-    description,
-    photo,
+    idOfProduct,
+    nameOfProduct,
+    bigPhotoProduct,
     totalResult
   ) {
     const product = {
       quantity: quantityValue,
-      color: color,
+      color: colorOfProduct,
       size: sizeProduct,
-      id: id,
-      name: description,
-      photo: photo,
+      id: idOfProduct,
+      name: nameOfProduct,
+      photo: bigPhotoProduct,
       price: totalResult,
     };
-    console.log('item,', product.price)
     const car = product.price
+    console.log('name', nameOfProduct)
     setValor([...valor, car])
     setCart([...cart, product]);
+    setOpen(true);
   }
 
+  
   
   useEffect(() => {
     const valueTotal = valor.reduce((acc, acm) => acc += acm)
@@ -42,6 +50,12 @@ export const ProductProvider = ({ children }) => {
     setCart(newListProduct);
   }
 
+  function handleSearch(names) {
+    setCartItem(names)
+  }
+
+  
+
 
   return (
     <ProductContext.Provider
@@ -51,7 +65,11 @@ export const ProductProvider = ({ children }) => {
         setProducts,
         handleAddNewProduct,
         handleDeleteItem,
-        amount
+        amount,
+        open,
+        handleSearch,
+        title, setTitle,
+        cartItem, setCartItem
       }}
     >
       {children}
